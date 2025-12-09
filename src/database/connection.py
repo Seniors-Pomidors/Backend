a9b159d8a242fa1ce894c126fe448698135592ab
Mikhail <doctorfomincov2005@mail.ru>
2025-12-09 18:57:34 +0300

    fix: database connection string

--- src/database/connection.py
+++ src/database/connection.py
@@ -6,10 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",  # имя переменной окружения
-    "postgresql://postgres:mfomintsovz1503z@localhost:5432/prodpaldev"  # значение по умолчанию
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
